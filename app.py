ddd4fa78aa41038c4824640b4c6ce8cabf754131
jonszcz <jonszcz@gmail.com>
2020-10-05 15:50:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='Masterplan Dashboard'
 
 app.layout = html.Div([
     html.H6('Interactive Master Plan Dashboard'),
